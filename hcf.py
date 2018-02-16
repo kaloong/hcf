0bf26a43155577aed75d6dfa4d658515b1f78c7f
kaloong <16438208+kaloong@users.noreply.github.com>
2018-02-16 01:05:29 +0000

    changed the regex

--- hcf.py
+++ hcf.py
@@ -1,29 +1,39 @@
 #!/usr/bin/env python
 
+
+import re
+
 csv_file_1 = open( "test.csv",'r')
 csv_file_2 = open( "test2.csv",'r')
-
+delim = " "
 hosts = {}
+comment_re = re.compile(r'(\w+ ){3,}|\W{2,}', re.VERBOSE) 
 class Host: pass
 
 def parse_csv( f ):
     headings = f.readline()
-    header_key, header_data = headings.strip().split(" ")
+    col_key, col_data = headings.strip().split( delim )
 
     for line in f:
-        host_key, host_data = line.strip().split(" ")
-        if host_key not in hosts.keys():
-            host = Host()
-            setattr( host, header_data, host_data ) 
-            hosts[ host_key ] = host 
-        else:
-            host_tmp = hosts[ host_key ]
-            setattr( host_tmp, header_data, host_data )
-            hosts[ host_key ] = host_tmp
+        if not comment_re.match(line): 
+            ''' 
+            First column of each line is treated as key
+            '''
+            print line
+            host_key, host_data = line.strip().split( delim )
+            if host_key not in hosts.keys():
+                host = Host()
+                setattr( host, col_data, host_data ) 
+                hosts[ host_key ] = host 
+            else:
+                host_tmp = hosts[ host_key ]
+                setattr( host_tmp, col_data, host_data )
+                hosts[ host_key ] = host_tmp
 
 parse_csv(csv_file_1)
 parse_csv(csv_file_2)
 
-for i in sorted( hosts.keys() ):
-    print i, hosts[i].__dict__
+for host in sorted( hosts.keys() ):
+    pkg, app = hosts[ host ].__dict__.values()
+    print host , pkg, app
 
